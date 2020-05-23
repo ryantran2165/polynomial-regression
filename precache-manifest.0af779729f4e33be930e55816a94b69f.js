b986833f68cbaf8d18d6f2a000eb7499c4187f0e
ryan_tran <ryantran2165@gmail.com>
2020-05-23 14:58:16 -0700

    Updates

---
+++ precache-manifest.0af779729f4e33be930e55816a94b69f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce69edc25e2e12283b1fc06b6259131",
+    "url": "/polynomial-regression-gradient-descent/index.html"
+  },
+  {
+    "revision": "461c4fc1460e7d9e3427",
+    "url": "/polynomial-regression-gradient-descent/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "687eb59e4a62a00a90f5",
+    "url": "/polynomial-regression-gradient-descent/static/css/main.64597955.chunk.css"
+  },
+  {
+    "revision": "461c4fc1460e7d9e3427",
+    "url": "/polynomial-regression-gradient-descent/static/js/2.5b07622f.chunk.js"
+  },
+  {
+    "revision": "ebc14457535110648808604cc4b96810",
+    "url": "/polynomial-regression-gradient-descent/static/js/2.5b07622f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687eb59e4a62a00a90f5",
+    "url": "/polynomial-regression-gradient-descent/static/js/main.36660cc4.chunk.js"
+  },
+  {
+    "revision": "376035698a4a07f3d4df",
+    "url": "/polynomial-regression-gradient-descent/static/js/runtime-main.f214d251.js"
+  }
+]);
